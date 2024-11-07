1a65ab38eca2588689924b15e44c38fe428b1cd5
Ulrich Diedrichsen <uli@moinsen.dev>
2024-11-07 14:08:35 +0100

    No Orientation restriction

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:moinsen_physics/_index.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
-    (value) => runApp(
-      MoinsenPhysicsApp(),
-    ),
+  runApp(
+    MoinsenPhysicsApp(),
   );
 }
